16963b11f254f7004e3305506d11f687c419386c
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-10-08 18:32:32 -0400

    array find method done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(tmRec){
+    let result = undefined;
+    for (let item of tmRec){
+        if (wonSB(item)){
+            result = item.year;
+            break;
+        }
+    }
+    return result;
+}
+
+function wonSB(elem){
+    return (elem.result === "W");
+}
